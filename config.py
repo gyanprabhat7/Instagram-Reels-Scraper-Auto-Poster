6b64f9ab475b778f919178ae175236b16daef0c2
Avinash Rathod <28943172+avinashrathod0306@users.noreply.github.com>
2023-04-20 15:49:45 +0530

    Added Files

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+#Config Variables
+CURRENT_DIR = os.getcwd() + os.sep
+DOWNLOAD_DIR = CURRENT_DIR + 'downloads' + os.sep #Path of folder where files will be stored
+FETCH_LIMIT = 500
+USERNAME = "procodedreamer"
+PASSWORD = "AVI9924288666"
+ACCOUNTS = [
+    #"totalgaming_official",
+    #"carryminati",
+    #"techno_gamerz",
+    #"payalgamingg",
+    #"dynamo__gaming"
+    "noy_tawil1"
+]
+
+HASHTAGS = "#gaming #gamer #ps #playstation #videogames #game #xbox #games #twitch #fortnite #pc #memes #pcgaming #gamers #gamingcommunity #youtube #xboxone #gamergirl #nintendo #gta #callofduty #streamer #follow #pubg #videogame #esports #bhfyp #meme #twitchstreamer #art"
